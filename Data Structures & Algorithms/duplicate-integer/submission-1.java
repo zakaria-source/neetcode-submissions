9a937a9ed11463a0c3be8dd417968b251bd1c920
Zakaria Dbaba <111566321+zakaria-source@users.noreply.github.com>
2026-08-17 21:40:44 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
